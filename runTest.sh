bd5eaf3dd99ddf4d665854e38c9b3b01b5048a9a
Murali Vijayaraghavan <vmurali@sifive.com>
2019-02-06 11:51:34 -0800

    changed indicator of finish

--- runTest.sh
+++ runTest.sh
@@ -12,9 +12,9 @@ execute "./runElf.sh $path > system.out"
 execute "riscv64-unknown-elf-objdump --disassemble $path > $path.asm"
 fail_address=$(awk '/([[:alnum:]])* <fail>:/ {print $1}' $path.asm)
 pass_address=$(awk '/([[:alnum:]])* <pass>:/ {print $1}' $path.asm)
-execute "grep --color --quiet $fail_address system.out"
+grep --color --quiet "Fetched: $fail_address" system.out"
 failed=$?
-execute "grep --color --quiet $pass_address system.out"
+"grep --color --quiet "Fetched: $pass_address" system.out"
 passed=$?
 if [[ $failed == 1 && $passed == 0 ]]
 then
