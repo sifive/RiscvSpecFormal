4120c32b5e3848721bc89eee50dfb2fa8cb58be0
Larry Lee <larryl@gamma03.internal.sifive.com>
2019-02-22 20:33:48 -0800

    Fixed a number of errors in CSR register and System.sv. Also added an additional command flag to avoid recompiling Kami modules with doGenerate.

--- CSRRegister.sv
+++ CSRRegister.sv
@@ -5,27 +5,49 @@ module csr_register (
   input         CLK,
   input         RESET,
   input         in_read_csr_enable,
+  input         in_read_fcsr_enable,
   input         in_write_csr_enable,
   input         in_write_fcsr_enable,
   input [11:0]  in_read_csr_select,
   input [11:0]  in_write_csr_select,
   input [31:0]  in_write_csr_data,
   input [31:0]  in_write_fcsr_data,
-  output [31:0] out_read_csr_data
+  output [31:0] out_read_csr_data,
+  output [31:0] out_read_fcsr_data
 );
 
 parameter integer num_registers = 4096; // 2^12
 reg [31:0] registers [(num_registers - 1):0];
 
+parameter integer fflags_index = 1;
+parameter integer frm_index = 2;
 parameter integer fcsr_index = 3;
 
 // read operations.
-assign out_read_csr_data = registers [in_read_csr_select];
+// assign out_read_csr_data = registers [in_read_csr_select];
 // assign out_read_csr_data = registers [0];
-always @(posedge CLK)
+always // @(posedge CLK)
 begin
   $write("[CSRRegister] read csr sel %d\n", in_read_csr_select);
-  $write("[CSRRegister] read csr val %d\n", out_read_csr_data);
+  assign out_read_fcsr_data = registers [fcsr_index];
+  $write("[CSRRegister] read fcsr val %d\n", registers [fcsr_index]);
+  if (in_read_csr_select == fflags_index)
+  begin
+    // read the fflags field within the fcsr register.
+    // out_read_csr_data = (registers [fcsr_index] && 32'hFFFFFFE0);
+    // out_read_csr_data = (registers [fcsr_index] & 32'hFFFFFFE0);
+    out_read_csr_data = registers [fcsr_index];
+    $write("[CSRRegister] read fflags %d\n", out_read_csr_data);
+  end else if (in_read_csr_select == frm_index)
+  begin
+    // read the frm field within the fcsr register.
+    out_read_csr_data = ((registers [fcsr_index] >> 5) && 32'hFFFFFFF8);
+    $write("[CSRRegister] read frm %d\n", out_read_csr_data);
+  end else
+  begin
+    out_read_csr_data = registers [in_read_csr_select];
+    $write("[CSRRegister] read other csr %d\n", out_read_csr_data);
+  end
 end
 
 // write csr operation.
@@ -33,15 +55,30 @@ always @(posedge CLK)
 begin
   if (in_write_csr_enable && !RESET)
   begin
-    registers [in_write_csr_select] <= in_write_csr_data;
-    // registers [0] <= in_write_csr_data;
+    if (in_write_csr_select == fflags_index)
+    begin
+      // write to the fflags field within the fcsr register.
+      $write("[CSRRegister] write fflags: %x\n", ((registers [fcsr_index] & 32'hFFFFFFE0) | (in_write_csr_data && 31)));
+      registers [fcsr_index] = (registers [fcsr_index] & 32'hFFFFFFE0) | (in_write_csr_data && 31);
+    end else if (in_write_csr_select == frm_index)
+    begin
+      // write to the frm field within the fcsr register.
+      $write("[CSRRegister] write frm: %x\n", ((registers [fcsr_index] & 32'hFFFFFF1F) | ((in_write_csr_data && 7) << 5)));
+      registers [fcsr_index] = (registers [fcsr_index] & 32'hFFFFFF1F) | ((in_write_csr_data & 7) << 5);
+    end else
+    begin
+      $write("[CSRRegister] write default");
+      registers [in_write_csr_select] = in_write_csr_data;
+    end
     $write("[CSRRegister] write csr sel %d\n", in_write_csr_select);
     $write("[CSRRegister] write csr val %d\n", in_write_csr_data);
   end
 
   if (in_write_fcsr_enable && !RESET)
   begin
-    registers [fcsr_index] <= in_write_fcsr_data;
+    registers [fcsr_index] = in_write_fcsr_data;
+    $write("[CSRRegister] write fcsr: %x\n", in_write_fcsr_data);
   end
+  $write("[CSRRegister] fcsr new val %d\n", registers [fcsr_index]);
 end
 endmodule
--- System.sv
+++ System.sv
@@ -20,14 +20,14 @@ logic fetch_enable;
 logic[31:0] fetch_address_req;
 
 struct packed {
-  logic[31:0] fst;
+  logic[31:0] inst;
   struct packed {
     logic valid;
     struct packed {
       logic[3:0] exception;
       logic[31:0] value;
     } data;
-  } snd;
+  } exception;
 } fetch_res;
 
 // Register read wires.
@@ -77,6 +77,9 @@ wire logic read_csr_en_req;
 wire logic [11:0] read_csr_sel_req;
 wire logic [31:0] read_csr_data_res;
 
+wire logic read_fcsr_en_req;
+wire logic [31:0] read_fcsr_data_res;
+
 // CSR write wires.
 
 wire logic write_csr_en_req;
@@ -95,17 +98,12 @@ wire logic[31:0] memRead_address_req;
 wire logic memRead_enable_req;
 
 struct packed {
-  struct packed {
-    logic[31:0] data;
-    logic[1:0] reservation;
-  } fst;
+  logic[31:0] data;
+  logic[1:0] reservation;
   struct packed {
     logic valid;
-    struct packed {
-      logic[3:0] exception;
-      logic [31:0] value;
-    } data;
-  } snd;
+    logic[3:0] data;
+  } exception$;
 } memRead_res;
 
 struct packed {
@@ -117,10 +115,7 @@ wire logic memWrite_enable_req;
 
 struct packed {
   logic valid;
-  struct packed {
-    logic[3:0] exception;
-    logic [31:0] value;
-  } data;
+  logic[3:0] data;
 } memWrite_res;
 
 // System components and connections
@@ -133,6 +128,7 @@ top system (
   .proc_core_read_freg_2$_return(read_freg_2_res),
   .proc_core_read_freg_3$_return(read_freg_3_res),
   .proc_core_read_csr$_return(read_csr_data_res),
+  .proc_core_read_fcsr$_return(read_fcsr_data_res),
   .proc_core_memRead$_return(memRead_res),
   .proc_core_memWrite$_return(memWrite_res),
 
@@ -149,6 +145,7 @@ top system (
   .proc_core_fregWrite$_argument(proc_core_fregWrite_req),
   .proc_core_csrWrite$_argument(proc_core_csrWrite_req),
   .proc_core_fcsrWrite$_argument(write_fcsr_data_req),
+  .proc_core_pc$_argument(pc),
   .proc_core_fetch$_enable(fetch_enable),
   .proc_core_read_reg_1$_enable(read_reg_1_enable_req),
   .proc_core_read_reg_2$_enable(read_reg_2_enable_req),
@@ -156,26 +153,18 @@ top system (
   .proc_core_read_freg_2$_enable(read_freg_2_enable_req),
   .proc_core_read_freg_3$_enable(read_freg_3_enable_req),
   .proc_core_read_csr$_enable(read_csr_en_req),
+  .proc_core_read_fcsr$_enable(read_fcsr_en_req),
   .proc_core_memRead$_enable(memRead_enable_req),
   .proc_core_memWrite$_enable(memWrite_enable_req),
   .proc_core_regWrite$_enable(proc_core_regWrite_enable_req),
   .proc_core_fregWrite$_enable(proc_core_fregWrite_enable_req),
   .proc_core_csrWrite$_enable(write_csr_en_req),
   .proc_core_fcsrWrite$_enable(write_fcsr_en_req),
-
-  .proc_core_pc$_argument(pc),
   .proc_core_pc$_enable(pc_enable),
 
   .CLK(CLK),
   .RESET(RESET)
 );
-
-always @(posedge CLK)
-begin
-  $write ("[System] read csr en %b\n", read_csr_en_req);
-  $write ("[System] read csr sel %d\n", read_csr_sel_req);
-  $write ("[System] read csr val %d\n", read_csr_data_res);
-end
 
 (* TODO: wire up exceptions. *)
 
@@ -192,10 +181,10 @@ memory32 ram (
   .in_read_address (memRead_address_req),
   .in_write_address (memWrite_req.addr),
   .in_write_data (memWrite_req.data),
-  .out_fetch_data (fetch_res.fst),
-  .out_read_data (memRead_res.fst.data),
-  .out_reservation (memRead_res.fst.reservation),
-  .out_fetch_exception (fetch_res.snd),
+  .out_fetch_data (fetch_res.inst),
+  .out_read_data (memRead_res.data),
+  .out_reservation (memRead_res.reservation),
+  .out_fetch_exception (ram_void1),
   .out_read_exception (ram_void2),
   .out_write_exception (ram_void3)
 );
@@ -235,13 +224,22 @@ csr_register csr_registers (
   .CLK (CLK),
   .RESET (RESET),
   .in_read_csr_enable (read_csr_en_req),
+  .in_read_fcsr_enable (read_fcsr_en_req),
   .in_write_csr_enable (write_csr_en_req),
   .in_write_fcsr_enable (write_fcsr_en_req),
   .in_read_csr_select (read_csr_sel_req),
   .in_write_csr_select (proc_core_csrWrite_req.index),
   .in_write_csr_data (proc_core_csrWrite_req.data),
   .in_write_fcsr_data (write_fcsr_data_req),
-  .out_read_csr_data (read_csr_data_res)
+  .out_read_csr_data (read_csr_data_res),
+  .out_read_fcsr_data (read_fcsr_data_res)
 );
+
+always @(posedge CLK)
+begin
+  $write ("[System] read csr en %b\n", read_csr_en_req);
+  $write ("[System] read csr sel %d\n", read_csr_sel_req);
+  $write ("[System] read csr val %d\n", read_csr_data_res);
+end
 
 endmodule
