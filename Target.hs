280cb81bcdab86e7c718b6beec8d5649a483b67a
Murali Vijayaraghavan <vmurali@sifive.com>
2019-04-27 18:39:53 -0700

    removed riscv-tests

---
+++ Target.hs
@@ -0,0 +1,10 @@
+module Target (module Syntax, module Rtl, module Word, module Fin, module EclecticLib, module PeanoNat, rtlMod) where
+import EclecticLib
+import PeanoNat
+import Fin
+import Instance
+import Rtl
+import Syntax hiding (unsafeCoerce)
+import Word
+rtlMod :: RtlModule
+rtlMod =
--- doGenerate.sh
+++ doGenerate.sh
@@ -120,20 +120,7 @@ else
   execute "$cmd"
 fi
 
-cat > Target.hs <<- EOF
-module Target (module Syntax, module Rtl, module Word, module Fin, module EclecticLib, module PeanoNat, rtlMod) where
-
-import EclecticLib
-import PeanoNat
-import Fin
-import Instance
-import Rtl
-import Syntax hiding (unsafeCoerce)
-import Word
-
-rtlMod :: RtlModule
-rtlMod = model$xlen
-EOF
+echo $xlen >> Target.hs
 
 if [[ $tune_ghc == 1 ]]
 then
