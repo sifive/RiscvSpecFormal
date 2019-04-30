92c2e3dcc600535a21c91189a9c3cb391d51e2b6
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2019-04-30 09:11:13 -0700

    test

--- createObjDir.sh
+++ createObjDir.sh
@@ -3,8 +3,9 @@
 echo $1
 
 cat Target.raw > Target.hs
-echo 'model = model$1' >> Target.hs
-ghc -j + RTS -A128m -n4m -O0 --make Kami/PrettyPrintVerilog.hs
+echo 'rtlMod = model$1' >> Target.hs
+#ghc -j + RTS -A128m -n4m -O0 --make Kami/PrettyPrintVerilog.hs
+ghc -O0 --make Kami/PrettyPrintVerilog.hs
 ./Kami/PrettyPrintVerilog > System.sv
 verilator --top-module system -Wno-CMPCONST -O0 -Wno-WIDTH --cc System.sv --trace --trace-underscore -Wno-fatal --exe System.cpp
 make -j -C obj_dir -f Vsystem.mk Vsystem CXX=clang LINK=clang
