af4d7dfed40a1c61b60a2dc49603d6728f3b0b7d
Murali Vijayaraghavan <vmurali@sifive.com>
2019-04-30 18:25:18 -0700

    stuff

---
+++ .travis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:hvr/ghc --yes
+sudo apt-get update
+sudo apt-get install ghc-8.6.5 clang --ye
+
+wget https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.2.0-2019.02.0-x86_64-linux-ubuntu14.tar.gz && tar xzf riscv64-unknown-elf-gcc-8.2.0-2019.02.0-x86_64-linux-ubuntu14.tar.gz
+
+git clone https://github.com/riscv/riscv-tests && cd riscv-tests && git submodule update --init --recursive && autoconf && ./configure --prefix=$HOME/riscv && sed -i -e \"s/0x80000000/00000000/g\" ./env/p/link.ld && make -j && make install && cd .. && rm -rf riscv-tests
+
+cp $HOME/haskell-files/*.hs ."
+
+cat Target.raw > Target.hs
+echo "rtlMod = model$1" >> Target.hs
+cat Target.hs
+ghc -O0 --make Kami/PrettyPrintVerilog.hs
+./Kami/PrettyPrintVerilog > System.sv
+
+verilator --top-module system -Wno-CMPCONST -O0 -Wno-WIDTH --cc System.sv --trace --trace-underscore -Wno-fatal --exe System.cpp
+make -j -C obj_dir -f Vsystem.mk Vsystem CXX=clang LINK=clang
+
