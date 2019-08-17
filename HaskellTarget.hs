4c0b11e848fedca3c8a4415fba539d3974364efa
Murali Vijayaraghavan <vmurali@sifive.com>
2019-08-16 19:25:19 -0700

    haskell target

---
+++ HaskellTarget.hs
@@ -0,0 +1,7 @@
+module HaskellTarget (module Syntax, module Word, module Fin, module EclecticLib, module PeanoNat, kami_model32, kami_model64) where
+import EclecticLib hiding (__)
+import PeanoNat
+import Fin
+import Instance
+import Syntax hiding (unsafeCoerce, __)
+import Word
