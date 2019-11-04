7c61a435930cc95b7a2712e5234574f121297530
Murali Vijayaraghavan <vmurali@sifive.com>
2019-11-04 15:02:56 -0800

    bumping all, adding a script to run just 8 tests

---
+++ simpleTests.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+( \
+	./doGenerate.sh --haskell --parallel && \
+	( \
+		./runElf.sh --xlen 64 --haskell --path $1/rv64ui-p-add & \
+		./runElf.sh --xlen 64 --haskell --path $1/rv64ui-v-add & \
+		./runElf.sh --xlen 32 --haskell --path $1/rv32ui-p-add & \
+		./runElf.sh --xlen 32 --haskell --path $1/rv32ui-v-add
+	) \
+) & \
+( \
+	( \
+		./doGenerate.sh --xlen 64 --parallel && \
+		( \
+			./runElf.sh --xlen 64 --path $1/rv64ui-p-add & \
+			./runElf.sh --xlen 64 --path $1/rv64ui-v-add \
+		) \
+	) && \
+	( \
+		./doGenerate.sh --xlen 32 --parallel && \
+		( \
+			./runElf.sh --xlen 32 --path $1/rv32ui-p-add & \
+			./runElf.sh --xlen 32 --path $1/rv32ui-v-add \
+		) \
+	) \
+)
