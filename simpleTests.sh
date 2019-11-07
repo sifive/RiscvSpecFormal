80ea9c525042811eabfac039b65ce582d32cdca6
Murali Vijayaraghavan <vmurali@sifive.com>
2019-11-06 19:27:24 -0800

    bumping kami

--- simpleTests.sh
+++ simpleTests.sh
@@ -14,18 +14,16 @@ test=$3
 	) \
 ) && \
 ( \
+	./doGenerate.sh --xlen 64 --parallel && \
 	( \
-		./doGenerate.sh --xlen 64 --parallel && \
-		( \
-			./runElf.sh --xlen 64 --path $1/rv64$pre-p-$test & \
-			./runElf.sh --xlen 64 --path $1/rv64$pre-v-$test \
-		) \
-	) && \
+		./runElf.sh --xlen 64 --path $1/rv64$pre-p-$test & \
+		./runElf.sh --xlen 64 --path $1/rv64$pre-v-$test &\
+	) \
+) && \
+( \
+	./doGenerate.sh --xlen 32 --parallel && \
 	( \
-		./doGenerate.sh --xlen 32 --parallel && \
-		( \
-			./runElf.sh --xlen 32 --path $1/rv32$pre-p-$test & \
-			./runElf.sh --xlen 32 --path $1/rv32$pre-v-$test \
-		) \
+		./runElf.sh --xlen 32 --path $1/rv32$pre-p-$test & \
+		./runElf.sh --xlen 32 --path $1/rv32$pre-v-$test \
 	) \
 )
