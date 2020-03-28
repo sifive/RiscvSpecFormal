b8a3cc7212a845a0902fc588c005296deb9b7f0f
Evan Marzion <49920135+sifive-emarzion@users.noreply.github.com>
2020-03-27 17:04:59 -0700

    Coq sim api (#32)
    * porting changes to coq-sim
    
    * bumps

--- Haskell/CoqSim.hs
+++ Haskell/CoqSim.hs
@@ -1,8 +1,9 @@
 import qualified Simulator as S
 import qualified RegisterFile as R
 import Instance
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
 import qualified Data.BitVector as BV 
 import System.Exit
 import System.IO
@@ -17,6 +18,8 @@ import Data.BitVector as BV
 import Control.Exception
 import Data.IORef
 import UartDev
+import SimLib
+import Syntax (separateModRemove, getRules, getAllRegisters, Kind(..), Signature)
 
 hex_to_integer :: T.Text -> Integer
 hex_to_integer txt = case hexadecimal txt of
@@ -44,55 +47,12 @@ getArgVal name n = do
 mem_file :: String
 mem_file = "pMemFile"
 
-data SimEnvironment = SimEnvironment {
-  consoleUART :: IORef UART_NS16550A
-}
-
-mkInitSimEnv :: IO SimEnvironment
-mkInitSimEnv = do
-  uartRef <- newIORef mkUART
-  return $ SimEnvironment uartRef
-
 console_read :: IO String
 console_read = do
   console_has_input <- try (hReady stdin) :: IO (Either IOError Bool)
   case console_has_input of
     Left _ -> return ""
     Right has_input -> if has_input then getLine else return ""
-
-simEnvPre :: SimEnvironment -> IO ()
-simEnvPre simEnv = do
-  consoleInput <- console_read
-  currUARTState <- readIORef $ consoleUART simEnv
-  let (consoleOutput, nextUARTState) = uart_deq_output (uart_enq_input currUARTState consoleInput) in do
-    when (consoleOutput /= "") $ do
-      consoleWriteRes <- try (putStrLn $ "[console out] > " ++ consoleOutput) :: IO (Either IOError ())
-      case consoleWriteRes of
-        Left errorMsg -> putStrLn $ "[Main] Error: an IO error occured while trying to print output to the console. " ++ show errorMsg
-        Right _ -> return ()
-    writeIORef (consoleUART simEnv) nextUARTState 
-
-env :: S.Environment SimEnvironment
-env = S.Build_Environment 
-    (\simEnv _ _ _ -> unsafeCoerce $ do
-      simEnvPre simEnv
-      return simEnv :: IO SimEnvironment)
-    (\simEnv (R.Build_FileState methods int_regs files) _ ruleName -> unsafeCoerce $ do
-        when (ruleName /= "proc_core_pipeline") (return ()) -- TODO: LLEE: increment timeout counter.
-        case M.lookup mem_file (unsafeCoerce files) of
-            Nothing -> error $ "File not found." ++ mem_file
-            Just (R.Build_RegFile _ _ _ _ _ _ _ v) -> do
-                sz <- isa_size
-                tohost_addr <- getArgVal "tohost_address" sz
-                let val = (unsafeCoerce v) V.! tohost_addr
-                let val' = ((unsafeCoerce val) :: BV.BV)
-                if val' == 1 then do
-                    hPutStrLn stderr "PASSED."
-                    exitSuccess
-                else if val' > 1 then do
-                    hPutStrLn stderr "FAILED."
-                    exitFailure
-                else return simEnv)
 
 binary_split :: Eq a => a -> [a] -> Maybe ([a],[a])
 binary_split x xs = go xs [] where
@@ -103,10 +63,11 @@ process_args :: [String] -> [(String,String)]
 process_args = catMaybes . map (binary_split '=')
 
 timeout :: Int
-timeout = 200000
+timeout = 2000000
 
-proc_core_readUART :: (BV.BV, (BV.BV, ())) -> fileState -> regs -> SimEnvironment -> IO (SimEnvironment, BV.BV)
-proc_core_readUART (addr, (size, _)) _ _ simEnv = do
+{-
+proc_core_readUART :: (BV.BV, (BV.BV, ())) -> fileState -> regs -> IO BV.BV
+proc_core_readUART (addr, (size, _)) _ _ = do
     result <- foldM
                 (\acc offset -> do
                   currUARTState <- readIORef $ consoleUART simEnv
@@ -130,15 +91,79 @@ proc_core_writeUART (addr, (val, (size, _))) _ _ simEnv = do
               (fromIntegral $ BV.nat $ BV.least 8 (val >>. (bitVec 7 (offset * 8)))))
       [0 .. (2 ^ (BV.nat $ size) - 1)]
     return (simEnv, BV.nil)
+-}
 
-main :: IO()
+proc_core_ext_interrupt_pending :: R.Any -> S.KamiState -> IO R.Any
+proc_core_ext_interrupt_pending _ _ = return $ unsafeCoerce False
+
+methods :: M.Map String (Syntax.Signature,S.Coq_meth_sig)
+methods = M.fromList [
+    ("proc_core_ext_interrupt_pending", ((Syntax.Bit 0,Syntax.Bool), proc_core_ext_interrupt_pending))
+  ]
+
+data Stream a = (:+) a (Stream a) deriving Show
+
+unwind_list :: [a] -> Stream a
+unwind_list xs = go xs xs
+
+  where
+
+    go xs [] = go xs xs
+    go xs (y:ys) = y :+ go xs ys
+
+data Outcome = Pass | Fail | Neither
+
+check_tohost :: S.KamiState -> IO Outcome
+check_tohost s = do
+  sz <- isa_size
+  tohost_addr <- getArgVal "tohost_address" sz
+  val <- getRegFileVal s mem_file tohost_addr
+  let val' = unsafeCoerce val :: BV.BV
+  return $ if val' == 1 then Pass else if val' > 1 then Fail else Neither
+
+main :: IO ()
 main = do
-    args <- getArgs
-    let files = process_args args
-    sz <- isa_size
-    let sim = unsafeCoerce (if sz == 32 then coqSim_32 else coqSim_64)
-    initSimEnv <- mkInitSimEnv
-    sim env initSimEnv files timeout
-      (\_ _ _ _ -> return (initSimEnv, False) :: IO (SimEnvironment, Bool)) -- proc_core_pipeline
-      proc_core_readUART
-      proc_core_writeUART
+  args <- getArgs
+  let files = process_args args
+  sz <- isa_size
+  ct <- newIORef 0
+  let mod = if sz == 32 then model32 else model64
+  let (_,(rfs,basemod)) = separateModRemove mod
+  let rules = unwind_list $ getRules basemod
+  state <- initialize mod files
+  go ct rules state
+
+    where
+
+      go ct (r :+ rules') state = do
+        currCt <- readIORef ct
+        when (currCt == timeout) $ do
+          hPutStrLn stderr "TIMEOUT TIMEOUT TIMEOUT TIMEOUT TIMEOUT TIMEOUT TIMEOUT TIMEOUT TIMEOUT."
+          exitFailure
+        writeIORef ct $ currCt + 1
+        state' <- simulate methods r state
+        outcome <- check_tohost state'
+        case outcome of
+          Neither -> go ct rules' state'
+          Pass -> do
+            hPutStrLn stderr "PASSED."
+            exitSuccess
+          Fail -> do
+            hPutStrLn stderr "FAILED FAILED FAILED FAILED FAILED FAILED FAILED FAILED FAILED."
+            exitFailure
+
+
+
+
+-- main :: IO()
+-- main = do
+--     args <- getArgs
+--     let files = process_args args
+--     sz <- isa_size
+--     let sim = unsafeCoerce (if sz == 32 then coqSim_32 else coqSim_64)
+--     initSimEnv <- mkInitSimEnv
+--     sim env initSimEnv files timeout
+--       (\_ _ _ _ -> return (initSimEnv, False) :: IO (SimEnvironment, Bool)) -- proc_core_pipeline
+--       proc_core_readUART
+--       proc_core_writeUART
+
