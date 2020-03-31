7dd703aa31aa013f135c208fd13534b610714dd4
Larry Darryl Lee Jr <llee454@gmail.com>
2020-03-31 16:51:43 -0400

    Issue 304 (#33)
    * Drafted the runTestsUsingSlurm.sh script.
    
    * Switch the runTestsUsingSlurm script to runnable.
    
    * Fixed a bug in runTestsUsingSlurm.sh.
    
    * Revised the memory limit used in the runTestsUsingSlurm script. The new limit works for all p tests and most v tests and represents a temporary compromise.
    
    * Revised the runTestsUsingSlurm script so that it appends the simulator used in the name of each test log.
    
    * Drafted the runTestsUsingSlurm.sh script.
    
    * Switch the runTestsUsingSlurm script to runnable.
    
    * Fixed a bug in runTestsUsingSlurm.sh.
    
    * Revised the memory limit used in the runTestsUsingSlurm script. The new limit works for all p tests and most v tests and represents a temporary compromise.
    
    * Revised the runTestsUsingSlurm script so that it appends the simulator used in the name of each test log.
    
    * Extended the help text to include useful usage hints.
    
    * Extended the help text to include useful usage hints.
    
    * Increased the memory limit to get more v tests to pass.
    
    * Increased memory size.
    
    * Issue 304: modified runTestsUsingSlurm so that it assigns a smaller memory limit to p tests.

---
+++ runTestsUsingSlurm.sh
@@ -0,0 +1,106 @@
+# This script generates a Slurm job that runs runElf for most
+# possible runElf configurations.
+
+source common.sh
+
+slurmNumCpus='2'
+slurmMinMemSize='3G'
+slurmMaxMemSize='15G'
+testDir='/nettmp/netapp1a/vmurali/riscv-tests/isa'
+resultsDir='runTestsUsingSlurmResults'
+sims=('coq-sim' 'haskell-sim')
+xlens=('64' '32')
+
+options=$(getopt --options="h" --longoptions="help" -- "$@")
+[ $? == 0 ] || error "Invalid command line. The command line includes one or more invalid command line parameters."
+
+eval set -- "$options"
+while true
+do
+  case "$1" in
+    -h | --help)
+      cat <<- EOF
+Usage
+-----
+
+ ./runTestsUsingSlurm.sh [OPTIONS]
+
+SUMMARY
+-------
+
+For every possible combination of XLEN and simulator (excluding
+Verilog) and RISCV test, this script generates a SLURM job that runs
+the test using runElf with the selected configuration parameters
+and stores the results in runTestsUsingSlurmResults.
+
+Each time these jobs are invoked, they append their log output to
+their associated log file.
+
+A summary of the job results can be found in
+runTestsUsingSlurmResults/summary.out. 0 indicates success. 1
+indicates that the test failed. 137 indicates that the slurm job
+exceeded its memory limit. Other code indicate failure.
+
+You can observe the slurm queue using:
+
+  watch squeue -u USERNAME
+
+You can observe the test results using:
+
+  watch tail --lines 40 runTestsUsingSlurmResults/summary.out
+
+OPTIONS
+-------
+
+  -h|--help
+  Displays this message.
+
+AUTHORS
+-------
+
+* Murali Vijayaraghavan
+* Larry Lee
+EOF
+      exit 0;;
+    --)
+      shift
+      break;;
+  esac
+done
+shift $((OPTIND - 1))
+
+mkdir -p $resultsDir
+
+function isPTest () {
+  local testName=$1
+  [[ ${testName:7:1} == 'p' ]]
+}
+
+function getMemLimit () {
+  local testName=$1
+  if isPTest $testName
+  then echo $slurmMinMemSize
+  else echo $slurmMaxMemSize
+  fi
+}
+
+function runTest () {
+  local sim=$1
+  local xlen=$2
+  local testName=$3
+  local cmd="srun --cpus-per-task=$slurmNumCpus --mem=$(getMemLimit $testName) runElf.sh --verbose --$sim --xlen $xlen --path $testDir/$testName >> $resultsDir/$testName-$sim.out 2>&1; echo \"\$? $testName $xlen $sim\" >> $resultsDir/summary.out" 
+  execute "$cmd"
+}
+
+for sim in ${sims[@]}
+do
+  for xlen in ${xlens[@]}
+  do
+    testNames=$(./blackList.sh $testDir $xlen)
+    for testName in $testNames
+    do
+      runTest $sim $xlen $(basename $testName) &
+      true
+    done
+  done
+done
